f3bccf7da33b5a3a785631005db70148bd40b6b1
Jasjot Singh <jasjot784@gmail.com>
2020-07-27 22:10:08 +0530

    Coroutine set up and click on start button implemented but data i snot displayed

--- app/src/main/java/com/jasjotsingh/trackmysleep/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/jasjotsingh/trackmysleep/sleeptracker/SleepTrackerViewModel.kt
@@ -1,11 +1,52 @@
 package com.jasjotsingh.trackmysleep.sleeptracker
 
 import android.app.Application
+import android.provider.SyncStateContract.Helpers.insert
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import com.jasjotsingh.trackmysleep.database.SleepDatabaseDao
+import com.jasjotsingh.trackmysleep.database.SleepNight
+import kotlinx.coroutines.*
 
 class SleepTrackerViewModel(
     val database: SleepDatabaseDao,
     application: Application
 ) : AndroidViewModel(application) {
+    private var viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+    private var tonight = MutableLiveData<SleepNight?>()
+    init {
+        initializeTonight()
+    }
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+    private fun initializeTonight() {
+        uiScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+        return withContext(Dispatchers.IO) {
+            var night = database.getTonight()
+            if (night?.endTimeMilli != night?.startTimeMilli) {
+                night = null
+            }
+            night
+        }
+    }
+    fun onStartTracking() {
+        uiScope.launch {
+            val newNight = SleepNight()
+            insert(newNight)
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+    private suspend fun insert(night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.insert(night)
+        }
+    }
+
 }
